5a687eafd89d161513f102039592cd9ef442321e
Saxon Brown <z5583002@ad.unsw.edu.au>
2025-09-26 17:39:56 +1000

    Added packages and code descriptions

--- PHYS3116CompMain.py
+++ PHYS3116CompMain.py
@@ -1,6 +1,7 @@
-import math
+# Added matplotlib package to plot and fit data, imported as plt to make using the package code easier #
 import matplotlib.pyplot as plt
+# Added pandas to allow python to read data from our .csv files and to make compiling our data together much easier #
 import pandas
+# Added numpy to allow for compilation and manipulation of arrays with our astronomical data which makes it easier to plot relationships #
 import numpy as np
 import astropy.io
-
