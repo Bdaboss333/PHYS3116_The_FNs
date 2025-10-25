d95f41aa2cbe19ddf76ff97159e35662fdaf1556
Saxon Brown <z5583002@ad.unsw.edu.au>
2025-10-25 12:53:40 +1100

    added my plots, saving before my laptop dies

--- TheFNsFinal.py
+++ TheFNsFinal.py
@@ -148,9 +148,121 @@ classification1 = k_and_v_results.drop(columns = ['Age_x','FeH_x','Age_err','Cla
 plt.show()
 print(classification1)
 
-# %% Classification Method 2
+# %% Classification Method 2 (Saxon)
+
+# ===== Import packages and define variables ==== #
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+harris_p1 = pd.read_csv('HarrisPartI.csv')
+harris_p3 = pd.read_csv('HarrisPartIII.csv')
+krause = pd.read_csv('Krause21_fixed.csv')
+vandenberg = pd.read_csv('vandenBerg_table2_fixed.csv')
+totmerge = pd.read_csv('totmerge.csv')
+totmerge2 = pd.read_csv('totmerge2.csv')
+
+ID_1 = totmerge['ID']
+x_1 = totmerge['X']
+y_1 = totmerge['Y']
+z_1 = totmerge['Z']
+ID_2 = totmerge2['ID']
+x_2 = totmerge2['X']
+y_2 = totmerge2['Y']
+z_2 = totmerge2['Z']
+
+r_c = totmerge2['r_c']
+sig_v = totmerge2['sig_v']
+ID_h = totmerge2['ID']
+Age_v = totmerge2['Age_x']
+
+ID=harris_p1['ID']
+R_gc=harris_p1['R_gc']
+Z=harris_p1['Z']
+X=harris_p1['X']
+Y=harris_p1['Y']
+r_c_h=harris_p3['r_c']
+v_r=harris_p3['v_r']
+
+FeH_x = totmerge2['FeH_x']
+Age_x = totmerge2['Age_x']
+FeH_y = totmerge2['FeH_y']
+Age_y = totmerge2['Age_y']
+
+# Made a plot of Galactocentric radius vs absolute height above the plane of the galaxy, as most of the in situ globular clusters are of decently similar distance, the accreted clusters should be pretty apparent #
+R = np.sqrt(X**2+Y**2)
+
+conditions = [v_r<100, (v_r>=100) & (v_r<=300), v_r>300]
+
+cond_colours = ['purple', 'red', 'green']
+
+point_colours = np.select(conditions, cond_colours, default = 'gray')
 
 
+plt.scatter(R, abs(Z), c=point_colours, alpha=0.3)
+plt.xlabel("Galactocentric Radius ($R = \sqrt{X^{2}+Y^{2}}$) (kpc)")
+plt.ylabel("Height from Plane (kpc)")
+plt.title("Galactocentric Radius vs Height over Galactic Plane")
+plt.plot([10, 10], [0, 110], linestyle='--', color='r', label="10kpc Certainty Range")
+plt.ylim(top=110, bottom=0.1)
+plt.xlim(left=0.1,right=100)
+plt.text(25,70,'# of Possibly Accreted Clusters = 61')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+conditions2 = [(X**2 + Y**2)**0.5 > 10, (X**2 + Y**2)**0.5 <= 10]
+
+cond_colours2 = ['blue', 'green']
+
+point_colours2 = np.select(conditions2, cond_colours2, default = 'gray')
+
+plt.scatter(X,Y,c=point_colours2, alpha=0.3)
+theta = np.linspace(0, 2*np.pi, 500)
+r = 10
+x = r * np.cos(theta)
+y = r * np.sin(theta)
+plt.plot(x, y, '--', c='red', lw=1.5, label='10kpc Certainty Range')
+plt.axis('equal')
+plt.xlabel('Galactic Coordinate X (kpc)')
+plt.ylabel('Galactic Coordinate Y (kpc)')
+plt.title('Galactic Coordinates X vs Y')
+plt.text(-95,22,'# of Possibly Accreted Clusters = 61')
+plt.legend()
+plt.grid(True, alpha=0.6)
+plt.show()
+
+conditions3 = [(x_2**2 + y_2**2)**0.5 > 9.8, (x_2**2 + y_2**2)**0.5 <= 9.8]
+
+cond_colours3 = ['blue', 'green']
+
+point_colours3 = np.select(conditions3, cond_colours3, default = 'gray')
+
+plt.scatter(FeH_x,Age_x,c=point_colours3,alpha=0.3)
+for i, txt in enumerate(ID_h):
+    if (x_2[i]**2 + y_2[i]**2)**0.5 > 9.8: 
+        plt.annotate(txt, (FeH_x[i], Age_x[i]), fontsize=8)
+plt.xlabel('[Fe/H]')
+plt.ylabel('Age (Gyr)')
+plt.title('Age vs Metallicity Plot of Krause21 Clusters')
+plt.text(-2.35,8.5,'# of Possibly Accreted Clusters = 19')
+plt.legend()
+plt.grid(True, alpha=0.6)
+plt.show()
+
+
+plt.scatter(FeH_y,Age_y,c=point_colours3,alpha=0.3)
+for i, txt in enumerate(ID_h):
+    if (x_2[i]**2 + y_2[i]**2)**0.5 > 9.8: 
+        plt.annotate(txt, (FeH_y[i], Age_y[i]), fontsize=8)
+plt.xlabel('[Fe/H]')
+plt.ylabel('Age (Gyr)')
+plt.title('Age vs Metallicity Plot of vandenBerg Clusters')
+plt.text(-2.35,9.5,'# of Possibly Accreted Clusters = 12')
+plt.legend()
+plt.grid(True, alpha=0.6)
+plt.show()
 
 # %% Classification Method 3
 
