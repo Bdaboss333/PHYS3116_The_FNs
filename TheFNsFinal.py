a357af15678935652fe01578c0ed29b440477262
Billy Baker <billy.baker1505@gmail.com>
2025-10-25 11:40:10 +1100

    made new final file and instructions for merge

--- TheFNsFinal.py
+++ TheFNsFinal.py
@@ -1,26 +1,24 @@
 """
-Just taking notes on paper I found
+Documentation here
 
-Classification of Accreted and In-Situ stars and cluster
-
-- Using Al/Fe ratio (which I don't think we have)
-- Generally, when plotting Age-Metallicity relationship (x=age,y=Fe/H), two tracks form,
-  one that goes straight up with age ~ 12.8 Gyr, and one that branches to younger ages
 """
 
+# Import libraries
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
 
-# Merging
+# %% Classification method 1 (Billy)
+
+# Read Krause and vandenBerg csv files
 krause = pd.read_csv('Krause21.csv')
 vandenberg = pd.read_csv('vandenBerg_table2.csv')
 
 # Rename Krause NGC ID column to 'NGC' in preparation for merging
 krause.rename(columns={'Object': '#NGC'}, inplace=True)
 
-# Append 'NGC' to start of each ID in vanden berg so that it can be merged with krause 
+# Append 'NGC' to start of each ID in vandenBerg so that it can be merged with krause 
 def renameID(ID):
     return 'NGC' + ID
 
@@ -30,10 +28,10 @@ vandenberg['#NGC'] = vandenberg['#NGC'].apply(renameID)
 krause_and_vandenberg_merged = pd.merge(krause,vandenberg,on='#NGC')
 
 # Include unique GCs which are missed by merge
-krause_and_vandenberg_unique = pd.concat([krause_and_vandenberg_merged,krause,vandenberg])
-k_and_v_complete = krause_and_vandenberg_unique.drop_duplicates(subset=['#NGC'])
+krause_and_vandenberg_w_unique = pd.concat([krause_and_vandenberg_merged,krause,vandenberg])
+k_and_v_complete = krause_and_vandenberg_w_unique.drop_duplicates(subset=['#NGC'])
 
-# Merge age and feh columns of unique gcs
+# Ensure that all data in FeH and Age columns are numeric or NaN
 pd.to_numeric(k_and_v_complete['FeH_x'],errors='coerce')
 pd.to_numeric(k_and_v_complete['FeH'],errors='coerce')
 pd.to_numeric(k_and_v_complete['Age_x'],errors='coerce')
@@ -131,8 +129,6 @@ for i in range(len(k_and_v_complete['#NGC'])):
 # Make table with just feh, age, age_err and classification
 k_and_v_results = k_and_v_complete.drop(columns = ['Mstar','rh','C5','Name','FeH_y','Age_y','Method','Figs','Range','HBtype','R_G','M_V','v_e0','log_sigma_0'])
 
-
-
 # Plot original graph but without errorbars and colour/shape based on whether accreted, in-situ or unsure
 plt.figure(2)
 plt.scatter(k_and_v_results['Age_x'][k_and_v_results['Classification']=='Accreted'],k_and_v_results['FeH_x'][k_and_v_results['Classification']=='Accreted'],c='r',s=15,marker='x',label='Accreted')  
@@ -146,8 +142,74 @@ plt.xlabel('Age (Gyr)')
 plt.legend()
 
 # Drop rest of columns so only NGC, Name and classification
-k_and_v_results = k_and_v_results.drop(columns = ['Age_x','FeH_x','Age_err','Class','AltName'])
+classification1 = k_and_v_results.drop(columns = ['Age_x','FeH_x','Age_err','Class','AltName'])
 
 # Show results
 plt.show()
-print(k_and_v_results)
+print(classification1)
+
+# %% Classification Method 2
+
+
+
+# %% Classification Method 3
+
+
+
+# %% Merging conditions to determine confidence in classifications
+
+# To merge, make sure all our tables have same column name for ngc/id and
+# that all gcs have same naming convention, then do following
+
+# Start by merging our 3 classification results tables
+
+# 2_classifications_merged = pd.merge(classification1,classification2,on='#NGC')
+
+# Then merge this with last classification
+
+# all_classifications_merged = pd.merge(2_classifications_merged,classification3,on='#NGC)
+
+
+# pd.merge merges tables but only includes things in #NGC column which are column across the
+# tables being merged, so to include the unique gcs that were missed by merge do this
+
+# classifications_w_unique = pd.concat([all_classifications_merged,classification1,classification2,classification3])
+# classifications_complete = classifications.drop_duplicates(subset=['#NGC'])
+
+# Now we have finalised table, which should have a column with ngcs/id, then three columns named
+# like Classification 1, Classification 2, etc. for classifactions 1 2 and 3, which are either 'Unsure', 
+# 'In-situ' or 'Accreted'
+
+# Then from this, use following function with .apply() to assign either -1 to in-situ, 0 to unsure and 1 to accreted
+
+def assign_value(classification):
+
+    if classification == 'In-situ':
+        return -1
+    elif classification == 'Accreted':
+        return 1
+    else:
+        return 0
+    
+# Then do the following to make copy of above table with classifications replaced with value from function
+
+# classification_values = classifications_complete['Classifcation 1':'Classification 3'].apply(assign_value)
+
+# If this doesn't work try this
+
+# classification_values = classifications_complete['Classifcation 1','Classificatoin 2','Classification 3'].apply(assign_value)
+
+# Now we should have table with ngc column and three columns with either 1, -1 or 0
+# Now take the mean across these columns to find the average value which we can use to determine our confidence in
+# our classification, as follows
+
+# classification_values['Classification 1':'Classification 3'] = np.mean(classification_values['Classification 1':'Classification 3'],axis=1)
+
+# Again this might not work, if so let me know and I'm happy to debug
+
+# Now we should have table with only 2 columns, one for ngc and one with a float between -1 and 1 i.e. 0.33
+# This number represents our percentage confidence that the gc is a certain classification, i.e. 0.33 means
+# we are 33% confident that the gc is accreted, and -0.33 means we are 33% sure that the gc is in-situ
+# because we assigned in-situ as negative and accreted as positive
+
+# Now present this data however you like, whether it's just the table or if it's in a plot up to you
